6aa993dfdf7be0d04e0ccbf2017777512a7a7a76
Kelvin Chen <kelvinyhchen@gmail.com>
2025-07-21 11:11:15 +1000

    Add Say table with methods to check blocklist

---
+++ RpFilter/Say.lua
@@ -0,0 +1,61 @@
+Say = {}
+
+Say.Blocklist = {
+    Drunkard    = {["Shire"] = true},
+    Townsperson = {["Shire"] = true, ["Bree-land"] = true, ["Bree-town"] = true},
+    Fisherman   = {["Ered Luin"] = true},
+    Dockworker  = {["Ered Luin"] = true},
+    Traveller   = {["Ered Luin"] = true},
+    Watcher     = {["Thorin's Hall"] = true},
+    Woodcutter  = {["Bree-land"] = true},
+    Bounder     = {["Bree-land"] = true},
+    Lalia       = {["Bree-land"] = true},
+    Prisoner    = {["Bree-town"] = true},
+    Minstrel    = {["Bree-town"] = true},
+    Farmer      = {["Bree-town"] = true},
+    Waitress    = {["Lone-lands"] = true},
+    Whittler    = {["Lone-lands"] = true},
+    Patron      = {["Lone-lands"] = true},
+    Idalene     = {["North Downs"] = true},
+    Ranger      = {["North Downs"] = true},
+    Dúrlammad   = {["North Downs"] = true},
+    Miner       = {["North Downs"] = true},
+    Smith       = {["North Downs"] = true},
+    ["Iris Goodbody"]       = {["Shire"] = true},
+    ["Tom Bombadil"]        = {["Bree-land"] = true},
+    ["Jim Skinner"]         = {["Bree-town"] = true},
+    ["Drunken Reveller"]    = {["Bree-town"] = true},
+}
+
+function Say:isAllowed(message)
+    local id, name = message:match("^<Select:IID:(0x%x+)>(.-)</Select>")
+
+    if not id then
+        return self:isFromLocalPlayer(message)
+    elseif self:isFromNpc(id) then
+        return self:isNpcAllowed(name)
+    else
+        -- Must be from another player
+        return true
+    end
+end
+
+function Say:isNpcAllowed(name)
+    local blockedNpc = self.Blocklist[name]
+    if not blockedNpc then
+        return true
+    elseif Location:isUnknown() then
+        return false
+    else
+        return not blockedNpc[Location:getRegion()]
+    end
+end
+
+function Say:isFromLocalPlayer(message)
+    return message:sub(1, 8) == "You say,"
+end
+
+function Say:isFromNpc(id)
+    -- "0x0346"
+    return id:sub(1, 11) == "0x034600005"
+end
