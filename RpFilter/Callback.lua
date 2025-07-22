5cd4d1b531c69b863ccf30ab00411a2ac3355b6e
Kelvin Chen <kelvinyhchen@gmail.com>
2025-07-22 12:33:22 +1000

    Remove unnecessary check

--- RpFilter/Callback.lua
+++ RpFilter/Callback.lua
@@ -12,18 +12,12 @@ function RemoveCallback(object, event, callback)
     if object[event] == callback then
         object[event] = nil
     elseif type(object[event]) == "table" then
-        for i = 1, #object[event] do
-            if object[event][i] == callback then
-                table.remove(object[event], i)
+        local callbacks = object[event]
+        for i = 1, #callbacks do
+            if callbacks[i] == callback then
+                table.remove(callbacks, i)
                 break
             end
-        end
-
-        local size = #object[event]
-        if size == 1 then
-            object[event] = object[event][1]
-        elseif size == 0 then
-            object[event] = nil
         end
     end
 end
